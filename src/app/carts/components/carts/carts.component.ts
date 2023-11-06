b698ae2891f4503124b662cf9d38cfb0ad66bea2
med363 <Vous@exemple.com>
2023-11-06 14:28:36 +0100

    generate pdf for order details

--- src/app/carts/components/carts/carts.component.ts
+++ src/app/carts/components/carts/carts.component.ts
@@ -1,6 +1,6 @@
 import { Component ,OnInit} from '@angular/core';
 import { CartsService } from '../../services/carts.service';
-
+import { jsPDF } from 'jspdf';
 @Component({
   selector: 'app-carts',
   templateUrl: './carts.component.html',
@@ -94,5 +94,47 @@ addCart() {
    })
 
    console.log(Model)
+
+   console.log('PDF is being generated...');
+     // Create a new instance of jsPDF
+  const doc = new jsPDF();
+
+ // Add a title to the PDF with custom styling
+ doc.setFontSize(20);
+ doc.text('Order Details', 105, 15, );
+
+  // Initialize the vertical position for content
+  let yPos = 30;
+  // Loop through the products in the cart
+  this.cartProduct.forEach((item, index) => {
+       // Product name and price with custom styling
+       doc.setFontSize(14);
+       doc.setTextColor(0, 0, 0); // Black color
+       doc.text(item.item.title, 20, yPos);
+       doc.text(`${item.item.price} DT`, 160, yPos);
+   
+       // Product quantity and total with custom styling
+       doc.setFontSize(12);
+       doc.setTextColor(128, 128, 128); // Gray color
+       doc.text(`Quantity: ${item.quantity}`, 20, yPos + 10);
+       doc.text(`Total: ${item.item.price * item.quantity} DT`, 160, yPos + 10);
+   
+
+
+    // Increase the vertical position for the next product
+    yPos += 30;
+  });
+
+ // Calculate the X-coordinate to center the text
+const textWidth = doc.getStringUnitWidth(`Total Price: ${this.total} DT`)  / doc.internal.scaleFactor;
+const xCoordinate = (doc.internal.pageSize.getWidth() - textWidth) / 2;
+
+// Add the total price with custom styling and center the text
+doc.setFontSize(16);
+doc.setTextColor(0, 0, 0); // Black color
+doc.text(`Total Price: ${this.total} DT`, xCoordinate, yPos + 20);
+  // Save the PDF as a file
+  doc.save('order_details.pdf');
+  }
  }
-}
+
